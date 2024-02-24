71c7c5e6367558453e5135520f7ac45b22e81eff
Ming Gu <93433791+MingGu1221@users.noreply.github.com>
2024-02-24 12:56:14 +0000

    Add files via upload

---
+++ LM_SPY_20_years_data.m
@@ -0,0 +1,105 @@
+clc;
+clear all;
+
+
+whole_data = readmatrix('C:\Users\Ming\Dropbox\ML AI DeepL Reinforcement\SPY_20_years_data.csv');
+%%%%%数据特点： 1 Date	2 Open	3 High	4 Low	5 Close	  6 Adj Close	7 Volume
+raw_data = whole_data(:, [2, 3, 4, 5]);
+%%%%%数据特点： 1 Open	2 High	3 Low	4 Close	
+
+% numRows = height(raw_data);
+% % 显示行数
+% disp(['Table contains ', num2str(numRows), ' rows.']);
+% %% 5033 rows
+
+%大概看看数据长啥样
+dataNormalized = zeros(size(raw_data));
+for i = 1:4
+    col = raw_data(:,i); % 获取第i列
+    dataNormalized(:,i) = (col - min(col)) ./ (max(col) - min(col)); % 列内归一化
+end
+% 绘制归一化数据
+figure; % 创建一个新的图形窗口
+hold on; % 保持当前图形，以便在同一图中绘制多条线
+colors = lines(4); % 生成14种不同的颜色
+for i = 1:4
+    plot(1:height(raw_data), dataNormalized(:,i), 'Color', colors(i,:), 'LineWidth', 0.3); % 绘制每列归一化数据
+end
+hold off;
+legend('Open', 'Hight', 'Low', 'Close', 'Volume'); % 自定义图例文本
+% 美化图表
+title('Normalized raw data')
+xlabel('Date days'); % 横坐标标签
+ylabel('Value'); % 纵坐标标签
+grid on; % 显示网格
+
+%%%%%%%%%%%%%%%%%%
+%用五分之四的数据进行训练， 用五分之一的数据进行预测
+%输入参数可以是1 Open	2 High	3 Low	4 Close	的某个或某几个或全部
+%输出参数是predictive close
+inputs = dataNormalized(:, 1:3);
+outputs = dataNormalized(:, 4);
+
+% 创建一个MLP前馈网络，这里假设有5个神经元在隐藏层
+% 你可以根据需要调整隐藏层神经元的数量
+net = feedforwardnet(5, 'trainlm');%%train-lm is referred as lm or Levenberg–Marquardt algorithm
+
+% 分割数据为训练集、验证集和测试集
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+
+% LM里 可以调节的参数初始阻尼因子 λ ，最大迭代次数，迭代精度，收敛步长
+net.trainParam.epochs = 99; % 设置最大迭代次数
+% 开始时，可以设置一个相对较大的epochs值以确保算法有足够的时间收敛。
+% 通过观察训练过程中的误差下降和验证集上的性能，
+% 你可以调整这个值以防止过拟合或不必要的计算。如果发现模型很快就收敛了，
+% 可以适当减少epochs；如果模型收敛太慢，可能需要增加epochs或调整其他参数。
+
+
+% 过拟合后果泛化能力差：模型在新的数据上的预测性能下降，表现为验证集和测试集上的误差明显高于训练集上的误差。
+net.trainParam.goal = 1e-3; % 设置训练目标精度
+% goal参数设置为一个较小的正数，表示训练误差的目标值。如果设置得太低，可能导致过拟合，
+% 因为模型会尝试过度拟合训练数据以达到非常低的误差率。开始时可以设置一个合理的目标精度，
+% 基于实际问题的复杂度和所需的准确度。在训练过程中，根据实际训练误差和验证误差调整此值。
+net.trainParam.mu_dec = 0.1; % LM算法中减小mu（阻尼因子）的比率
+net.trainParam.mu_inc = 10; % LM算法中增加mu的比率
+net.trainParam.mu_max = 1e10; % mu的最大值，阻尼因子的上限
+net.trainParam.showWindow = true; % 训练过程中是否显示训练窗口
+
+% 训练网络
+[net,tr] = train(net,inputs',outputs');
+
+% 使用训练好的网络进行预测
+outputsPredict = net(inputs');
+
+% 反归一化预测值以便于比较
+maxClose = max(raw_data(:,4));
+minClose = min(raw_data(:,4));
+outputsPredict = outputsPredict' * (maxClose - minClose) + minClose;  %%预测的输出量
+outputsReal = outputs' * (maxClose - minClose) + minClose; %%真实的输出量
+
+
+
+% 绘制实际值与预测值
+figure;
+% plot(outputs * (maxClose - minClose) + minClose, 'b');
+plot(outputsReal)
+hold on;
+plot(outputsPredict, 'r--');
+
+legend('Actual Close','Predicted Close');
+title('Stock Close Price Prediction');
+xlabel('Days');
+ylabel('Close Price');
+grid on;
+
+
+
+
+
+
+
+
+
+
---
+++ category_SVM_Floweer.m
@@ -0,0 +1,69 @@
+% SVM分类预测 is developed for one physical quantity distribution at a single
+% node independently
+
+clc;
+clear all;
+
+whole_data = readtable('C:\Users\Ming\Dropbox\ML AI DeepL Reinforcement\tableConvert.com_n0fwak.csv');
+% 显示前几行数据以检查
+% head(dataTable)
+% 每列代表着 sepal_length	sepal_width	  petal_length	  petal_width	species
+
+% 假设X是特征数据，每一行是一个样本，每一列是一个特征
+% 假设Y是标签向量，其中包含每个样本的类别标签
+% 例如，对于二分类问题，Y中的值可能为0和1
+
+% 特征数据 & 标签
+X = whole_data(:, 1: 4);
+Y = categorical(whole_data.species);
+
+% 训练集70%,
+% 使用cvpartition函数和其HoldOut参数，数据集被分割成两个互斥的子集：一个用于训练模型的训练集和一个用于评估模型性能的测试集。
+cv = cvpartition(Y, 'HoldOut', 0.3); % 保留30%的数据用于测试,分割数据集 没有validation集是因为后面用了k fold cross validation
+
+idxTrain = training(cv);%要训练的索引
+idxTest = test(cv);%要测试的
+
+% 待训练
+XTrainCV = X(idxTrain, :); %训练特征量
+YTrainCV = Y(idxTrain, :); %表征到type里
+
+XTest = X(idxTest, :);
+YTest = Y(idxTest, :);
+
+% 对于分类进行训练训练，计算k次迭代中指标(准确率)的平均，一般设置k=5or10
+k = 10;
+cv = cvpartition(YTrainCV, 'kfold', k);
+% 初始化变量来存储每一折的准确率
+accuracyList = zeros(cv.NumTestSets, 1);
+
+for i = 1:cv.NumTestSets
+    rng(1);  % 设置随机种子为1
+    disp(rand(1));  % 生成一个随机数
+
+    idxTrain = training(cv, i);%要训练的索引
+    idxValidation = test(cv, i);%要Validation的索引
+
+    XTrain = XTrainCV(idxTrain, :); %训练特征量
+    YTrain = YTrainCV(idxTrain, :); 
+
+    XValidation = XTrainCV(idxValidation, :);
+    YValidation = YTrainCV(idxValidation, :); %验证
+
+    % 多类分类： 将使用fitcecoc函数，它基于一对所有（One-vs-All）策略来训练逻辑回归模型。
+    t = templateLinear('Learner', 'logistic');
+    model = fitcecoc(XTrain, YTrain, 'Learners', t, 'Coding', 'onevsall', 'ObservationsIn', 'rows');
+    
+    % 使用训练好的模型对测试集进行预测。
+    YPred = predict(model, XTest, 'ObservationsIn', 'rows');
+
+% 计算准确率来使用 后续取平均
+    accuracy = sum(YPred == YTest) / numel(YTest);
+    fprintf('Accuracy: %.2f%%\n', accuracy * 100);
+end
+
+meanAccuracy = mean(accuracyList);
+fprintf('Mean Accuracy after k-fold: %.2f%%\n', accuracy * 100);
+
+
+
